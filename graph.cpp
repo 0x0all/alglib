878c3d9c996b0dd9c8ca53ce41f585a2fae4ad64
Brian <shaohuang@gmail.com>
2009-09-13 00:10:20 -0700

    Coding style, STL container uses type instead of pointers

--- graph.cpp
+++ graph.cpp
@@ -12,123 +12,106 @@
 
 using namespace std;
 
-Vertex::Vertex()
-{
-}
-
 void Vertex::set_id(int id)
 {
-	this->m_id = id;
+  this->m_id = id;
 }
 
 int Vertex::get_id(void) const
 {
-	return this->m_id;
+  return this->m_id;
 }
 
-Vertex::~Vertex()
+void Vertex::add_connect(Vertex& v, EdgeDirection direction)
 {
-	cout<< "Remove a vertex: "<< this->get_id() << endl;
-}
-
-void Vertex::add_connect(Vertex* v, EdgeDirection direction)
-{
-	if (direction == OUT_GOING)
-		m_outs.push_back(v);
-	else
-		m_ins.push_back(v);
+  if (direction == OUT_GOING)
+    m_outs.push_back(v);
+  else
+    m_ins.push_back(v);
 }
 
 /*
  *   FIXME: O(N) type of remove algorithm is not good
  */
-void Vertex::remove_connect(Vertex* v, EdgeDirection direction)
+void Vertex::remove_connect(const Vertex& v, EdgeDirection direction)
 {
-	if (direction == OUT_GOING)
-		m_outs.remove(v);
-	else
-		m_ins.remove(v);
+  if (direction == OUT_GOING)
+    m_outs.remove(v);
+  else
+    m_ins.remove(v);
 }
 
 int Vertex::get_edges(EdgeDirection direction)
 {
-	int ret;
-	if (direction == OUT_GOING)
-		ret = m_outs.size();
-	else
-		ret = m_ins.size();
-	
-	return ret;
+  int ret;
+
+  if (direction == OUT_GOING)
+    ret = m_outs.size();
+  else
+    ret = m_ins.size();
+
+  return ret;
 }
 
-Vertex* Vertex::front(EdgeDirection direction)
+Vertex& Vertex::front(EdgeDirection direction)
 {
-	Vertex* v;
-	switch (direction) {
-	case OUT_GOING:
-		v = this->m_outs.front();
-		break;
-	case IN_COMING:
-		v = this->m_ins.front();
-		break;
-	default:
-		v = NULL;
-		break;
-	}
-	return v;
+  if (direction == OUT_GOING)
+    return this->m_outs.front();
+  else
+    return this->m_ins.front();
 }
 
-vertices::iterator Vertex::get_begin(EdgeDirection direction)
+vertices::iterator Vertex::begin(EdgeDirection direction)
 {
-	vertices::iterator i;
-	switch (direction) {
-	case OUT_GOING:
-		i = this->m_outs.begin();
-		break;
-	case IN_COMING:
-		i = this->m_ins.begin();
-		break;
-	default:
-		i = NULL;
-		break;
-	}
+  v_iter i;
+  if (direction == OUT_GOING)
+    i = this->m_outs.begin();
+  else
+    i = this->m_ins.begin();
 
-	return i;
+  return i;
+}
+
+bool Vertex::operator==(const Vertex& u)
+{
+  if (this->m_id == u.m_id)
+    return true;
+  else
+    return false;
 }
 
 static void unit_test(void)
 {
-	Vertex *v = new Vertex;
-	v->set_id(0);
-	for (int i = 0; i < 3; ++i) {
-		Vertex *u = new Vertex;
-		v->add_connect(u, OUT_GOING);
-		u->set_id(i+1);
-	}
+  Vertex *v = new Vertex;
+  v->set_id(9);
+
+  for (int i = 0; i < 3; ++i) {
+    Vertex *u = new Vertex;
+    u->set_id(i+1);
+    v->add_connect(*u, OUT_GOING);
+  }
 	
-	cout << "Now have " << v->get_edges(OUT_GOING) << " edges"<< endl;
-	vertices::iterator it = v->get_begin(OUT_GOING);
-	for (int i = 0; i < v->get_edges(OUT_GOING); i++) {
-		cout << "IDs: "<<(*it)->get_id()<<endl;
-		it++;
-	}
+  cout << "Now have " << v->get_edges(OUT_GOING) << " edges"<< endl;
+  Vertex::v_iter it = v->begin(OUT_GOING);
+  for (int i = 0; i < v->get_edges(OUT_GOING); i++) {
+    cout << "IDs: "<< (*it).get_id() <<endl;
+    it++;
+  }
 	
+  while (v->get_edges(OUT_GOING) != 0) {
+    Vertex& u = v->front(OUT_GOING);
+    v->remove_connect(u, OUT_GOING);
+  }
 	
-	while (v->get_edges(OUT_GOING) != 0) {
-		Vertex* u = v->front(OUT_GOING);
-		v->remove_connect(u, OUT_GOING);
-		delete u;
-	}
-	
-	cout << "then have " << v->get_edges(OUT_GOING) << " edges"<< endl;
-	
-	if (v) {
-		delete v;
-	}
+  cout << "then have " << v->get_edges(OUT_GOING) << " edges"<< endl;
+
+  if (v) {
+    delete v;
+  }
 }
 
 int main(int argc, char* argv[])
 {
-	unit_test();
-	return 0;
+  unit_test();
+  return 0;
 }
--- graph.h
+++ graph.h
@@ -3,38 +3,42 @@
  * 
  *
  *  Created by BRIAN PIN on 9/3/09.
- *  Copyright 2009 PuzzleLogicInc. All rights reserved.
+ *  Copyright 2009 P. All rights reserved.
  *  We are a Puzzle Solving Company!
  *
  */
 
 
 typedef enum VertexEdgeDirection {
-	OUT_GOING = 0,
-	IN_COMING = 1,
+  OUT_GOING = 0,
+  IN_COMING = 1,
 } EdgeDirection;
 
 
 class Vertex;
-typedef std::list<Vertex *> vertices;
+typedef std::list<Vertex> vertices;
 
 class Vertex {
 public:
-	Vertex();
-	virtual		~Vertex();
+  Vertex() {}
+  virtual ~Vertex() {std::cout<<"recycling vertex id:"<< m_id <<std::endl;}
 
-	void		add_connect(Vertex* v, EdgeDirection direction);
-	void		remove_connect(Vertex* v, EdgeDirection direction);
-	Vertex*		front(EdgeDirection direction);
-	int		get_edges(EdgeDirection direction);
+  void          add_connect(Vertex& v, EdgeDirection direction);
+  void          remove_connect(const Vertex& v, EdgeDirection direction);
+  int           get_edges(EdgeDirection direction);
+
+  // operator overloads
+  bool          operator==(const Vertex& u);
 	
-	// iteration functions
-	vertices::iterator get_begin(EdgeDirection direction);
+  // connection iteration functions
+  typedef vertices::iterator v_iter;
+  v_iter        begin(EdgeDirection direction);
+  Vertex&       front(EdgeDirection direction);
 
-	void		set_id(int id);
-	int		get_id(void) const;
+  void          set_id(int id);
+  int           get_id(void) const;
 private:
-	int		m_id;
-	vertices	m_outs;
-	vertices	m_ins;
+  int           m_id;
+  vertices      m_outs;
+  vertices      m_ins;
 };
