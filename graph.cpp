388e73259a01209f93e3b765b6e8adfb8101e82b
Brian <shaohuang@gmail.com>
2009-10-07 09:25:09 -0700

    change the object type in list<Type> to pointer becasue this will not copy another instance of my vertex, which is what I expected

--- graph.cpp
+++ graph.cpp
@@ -12,15 +12,15 @@
 #include <iostream>
 #include "graph.h"
 
-
+#if 0
 // added but mem already been allocated
-void Graph::add_vertex(const Vertex& new_v)
+void Graph::add_vertex(Vertex& new_v)
 {
 	graph_vertices.push_back(new_v);
 }
 
 // deleted but mem not recycled
-void Graph::del_vertex(const Vertex& v)
+void Graph::del_vertex(const Vertex* v)
 {
 	vertices::iterator it;
 	
@@ -40,18 +40,33 @@ void Graph::del_vertex(const Vertex& v)
 	}
 }
 
-void build_connection(Vertex& parent, const Vertex& child)
+void Graph::build_connection(Vertex& parent, const Vertex& child)
 {
 	parent.add_connection(child);
 }
 
-void bfs_search(const Vertex& source)
+void Graph::bfs_search(const Vertex& source)
 {
-	// to be implemented
-}
+	std::list<Vertex> bfs_q;
+	vertices::iterator it;
 
+	// initialization
+	for (it = graph_vertices.begin(); it != graph_vertices.end(); it++) {
+		if (source != *it) {
+			it->set_dist((unsigned)(-1));
+			it->set_flag(WHITE);
+		}
+	}
+	
+	bfs_q.push_back(source);
+	while (true != bfs_q.empty()) {
+		bfs_q.erase(bfs_q.begin());
+	}
+}
+#endif
 int unit_test1(void)
 {
+	typedef std::list<Vertex*> vertices;
 	Vertex u("u");
 	Vertex v1("v1");
 	Vertex v2("v2");
@@ -60,10 +75,10 @@ int unit_test1(void)
 
 	Vertex u1;
 
-	u.add_connection(v1);
-	u.add_connection(v2);
-	u.add_connection(v3);
-	u.add_connection(v4);
+	u.add_connection(&v1);
+	u.add_connection(&v2);
+	u.add_connection(&v3);
+	u.add_connection(&v4);
 
 	u1 = u;
 	std::cout << "Name: " << u1.get_name() << std::endl;
@@ -71,7 +86,7 @@ int unit_test1(void)
 	for (it = u1.connection_begin(); 
 		 it != u1.connection_end();
 		 it ++) {
-		std::cout << "Child: " << it->get_name() <<std::endl;
+		std::cout << "Child: " << (*it)->get_name() <<std::endl;
 	}
 }
 
--- graph.h
+++ graph.h
@@ -6,7 +6,8 @@
  *	1. Simple, simplicity
  *  2. const_iterator and iterator,
  *  3. exception types
- *
+ *  4. mutable keyword
+ *  5. STL containers insert,erase,push_back,pop, will all use a copy of T
  */
 #ifndef __GRAPH_H__
 #define __GRAPH_H__
@@ -19,19 +20,20 @@ class VertexAssignException : public std::exception
 	}
 };
 
-class Vertex;
-typedef std::list<Vertex> vertices;
+enum VertexFlags {
+	WHITE = 1,
+	GREY = 2,
+	BLACK = 3,
+};
 
 class Vertex {
 public:
+	typedef std::list<Vertex*> vertices;
+	Vertex() { }
+	explicit Vertex(std::string aname) : name(aname)
+	{ std::cout<<name<<" created"<<std::endl; }
 
-	Vertex() {}
-	explicit Vertex(std::string aname)
-	{
-		name = aname;
-	}
-
-	virtual ~Vertex() {}
+	virtual ~Vertex() {std::cout<<name<<" deleted"<<std::endl;}
 	
 	//
 	// copy and assign
@@ -39,11 +41,13 @@ public:
 	Vertex(const Vertex& that_v)
 	{
 		name = that_v.name;
+		std::cout<<name<<" copied"<<std::endl;
 	}
 	
 	Vertex& operator=(const Vertex& that_v)
 	{
 		name = that_v.name;
+		std::cout<<name<<" assigned"<<std::endl;
 		if (true != connection_list.empty()) {
 			throw VertexAssignException();
 		}
@@ -55,7 +59,7 @@ public:
 	}
 
 	// compare, do I need it?
-	bool operator==(const Vertex& u)
+	bool operator==(const Vertex& u) const
 	{
 		if (this == &u && name == u.name)
 			return true;
@@ -63,7 +67,7 @@ public:
 			return false;
 	}
 
-	bool operator!=(const Vertex& u)
+	bool operator!=(const Vertex& u) const
 	{
 		return !(*this==u);
 	}
@@ -77,19 +81,19 @@ public:
 	//
 	//  Add and Remove child vertex operation
 	//
-	void add_connection(const Vertex& v)
+	void add_connection(Vertex* v)
 	{
-		connection_list.push_back(v);
+		connection_list.insert(connection_list.begin(), 1, v);
 	}
 	
-	void del_connection(const Vertex& v)
+	void del_connection(Vertex* v)
 	{
 		vertices::iterator it;
 		for (it = connection_list.begin(); it != connection_list.end();
 				it ++) {
 			if (*it == v) {
 				connection_list.erase(it);
-				std::cout << "remove " << it->get_name() << std::endl;
+				std::cout << "remove " << (*it)->get_name() << std::endl;
 			}
 		}
 	}
@@ -107,23 +111,46 @@ public:
 	{
 		return connection_list.end();
 	}
+	
+	void set_dist(unsigned dist)
+	{
+		dist_src = dist;
+	}
+	
+	unsigned	get_dist(void) const
+	{
+		return dist_src;
+	}
+	
+	void set_flag(unsigned f)
+	{
+		flag = f;
+	}
+	
+	unsigned get_flag(void) const
+	{
+		return flag;
+	}
 
 private:
-	std::string		name;
-	vertices		connection_list;
+	unsigned				flag;
+	unsigned				dist_src;
+	std::string				name;
+	mutable vertices		connection_list;
 };
 
-
+#if 0
 class Graph {
 public:
+	typedef std::list<Vertex> vertices;
 	Graph();
 	virtual ~Graph();
 	// copy and assign
 
 	// building and traversing
-	virtual void	add_vertex(const Vertex& new_v);
+	virtual void	add_vertex(Vertex& new_v);
 	virtual void	del_vertex(const Vertex& v);
-	virtual void	build_connection(const Vertex& parent, const Vertex& child);
+	virtual void	build_connection(Vertex& parent, const Vertex& child);
 	virtual void	bfs_search(const Vertex& source);
 private:
 	// disallow the copy constructor and assign operator
@@ -131,5 +158,5 @@ private:
 	Graph& operator=(const Graph& that_g);
 	vertices		graph_vertices;
 };
-
+#endif
 #endif // __GRAPH_H__
