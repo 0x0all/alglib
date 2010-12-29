e285d5dd85a076adf840d9694108bc506ab43944
SHAOHUANG PIN <shaohuang@SHAOHUANG-PINs-iMac.local>
2010-12-29 13:07:25 -0800

    Unit test for b-tree

---
+++ b-tree-test.py
@@ -0,0 +1,19 @@
+import b-tree
+import sys
+import unittest
+
+class TestBSTCase(unittest.TestCase):
+  def test_insert(self):
+    bst = BST()
+    bst.insert(6)
+    bst.insert(2)
+    bst.insert(9)
+    
+
+  def test_search(self):
+  def test_succes(self):
+  def test_predec(self):
+  def test_delete(self):
+
+if __name__ == '__main__':
+  unittest.main()
